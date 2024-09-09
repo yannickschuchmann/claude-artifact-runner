f86e7097d1c9ff5ff3985b327b45c6826409eda0
yannickschuchmann <yannick.schuchmann@googlemail.com>
2024-09-09 19:02:39 +0200

    Use hash route

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Router } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import App from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router base="/claude-artifact-runner">
+    <Router hook={useHashLocation}>
       <App />
     </Router>
   </React.StrictMode>
