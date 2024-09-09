c35f27564fcc28a20df08ee3a1c9309a10b1a193
yannickschuchmann <yannick.schuchmann@googlemail.com>
2024-09-09 19:21:04 +0200

    Set base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/claude-artifact-runner",
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      'src': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
+      src: path.resolve(__dirname, "./src"),
     },
-  }
-})
+  },
+});
