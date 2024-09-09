8bba35d94cd6c1ac2ed385acc99faa98a28c513f
yannickschuchmann <yannick.schuchmann@googlemail.com>
2024-09-09 19:15:59 +0200

    Use vite base path

--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,9 @@ import { useHashLocation } from "wouter/use-hash-location";
 import App from "./App.tsx";
 import "./index.css";
 
-const getBasePath = () => {
-  if (import.meta.env.PROD) {
-    // Extract repository name from the URL
-    const repoName = window.location.pathname.split('/')[1];
-    return `/${repoName}`;
-  }
-  return '/';
-};
-
-const base = getBasePath();
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router base={base} hook={useHashLocation}>
+    <Router base={import.meta.env.BASE_URL} hook={useHashLocation}>
       <App />
     </Router>
   </React.StrictMode>
